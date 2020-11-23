d5d42e61f20dda86acfad96b201557e90a307c92
leo8912 <474482084@qq.com>
2020-11-23 10:58:21 +0800

    Update diy-1.sh

--- build/Lienol/diy-1.sh
+++ build/Lienol/diy-1.sh
@@ -3,7 +3,7 @@
 #
 
 #添加自定义源
-# sed -i '$a src-git mypackages https://github.com/siropboy/mypackages' feeds.conf.default
+ sed -i '$a src-git mypackages https://github.com/siropboy/mypackages' feeds.conf.default
 
 #移除不用软件包
 #rm -rf xxx/xxx/xxx/文件名
@@ -12,11 +12,8 @@
 #添加自定义插件链接（自己想要什么就github里面搜索然后添加）
 git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/diy/luci-theme-edge  #主题-edge
 git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git package/diy/luci-theme-infinityfreedom  #透明主题
-#git clone -b master https://github.com/vernesong/OpenClash.git package/diy/luci-app-openclash  #openclash出国软件
-#git clone https://github.com/frainzy1477/luci-app-clash.git package/diy/luci-app-clash  #clash出国软件
 git clone https://github.com/tty228/luci-app-serverchan.git package/diy/luci-app-serverchan  #微信推送
-git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/diy/luci-app-smartdns  #smartdns DNS加速
-#git clone https://github.com/garypang13/luci-app-eqos.git package/diy/luci-app-eqos  #内网IP限速工具
+git clone https://github.com/garypang13/luci-app-eqos.git package/diy/luci-app-eqos  #内网IP限速工具
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/diy/luci-app-jd-dailybonus  #京东签到
 git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter  #应用程序过滤
 
