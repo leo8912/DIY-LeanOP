2d00983832812bb32322b97f1d2db50672538009
leo8912 <474482084@qq.com>
2020-11-27 18:58:15 +0800

    Update diy-1.sh

--- build/Lienol/diy-1.sh
+++ build/Lienol/diy-1.sh
@@ -33,3 +33,6 @@ svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/redsocks2 package
 rm -rf ./package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/diy/luci-theme-argon  #新的argon主题
 #全新的[argon-主题]此主题玩法很多,这里看说明【https://github.com/jerrykuku/luci-theme-argon/blob/18.06/README_ZH.md】
 #用WinSCP连接openwrt，在/www/luci-static/argon里面创建background文件夹（如果本身就有background就不需要创建）来存放jpg png gif格式图片可以自定义登陆界面，gif图片为动态登陆界面
+
+# 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
