5aa828a0d5030d76bfd4fda934cea7022ceb2649
leo8912 <474482084@qq.com>
2021-01-22 21:54:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,5 +15,3 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
-git clone https://github.com/281677160/openwrt-package package/danshui
