a1f97de78907d8e7ed677f48d9d6521a47662da9
leo8912 <474482084@qq.com>
2021-07-02 13:53:16 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,4 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git ssrp https://github.com/fw876/helloworld.git
