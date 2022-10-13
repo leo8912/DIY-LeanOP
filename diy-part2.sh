fcf035e89d17551596c5a4fb5b4f1c591cfbc2e9
Lao <474482084@qq.com>
2022-10-13 18:58:54 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.2.200/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.2.200/g' package/base-files/files/bin/config_generate
 
 
 
